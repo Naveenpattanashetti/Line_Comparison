2098f676df0c24800945f25271357558c7bb960a
Naveen Pattanashetti <pattanashettinaveen.com>
2021-09-28 21:02:41 +0530

    welcome to line comparison program

---
+++ src/com/bridgelabz/linecomparison/Linecomparison.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linecomparison;
+
+public class Linecomparison {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to line comparison program");
+
+	}
+
+}
